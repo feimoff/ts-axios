2ad8244454f4f6eca240c6c6ca863462503aaa91
热忱 <1664889768@qq.com>
2024-06-26 19:14:11 +0800

    02-全局实例拦截器

--- src/services/request/index.ts
+++ src/services/request/index.ts
@@ -6,6 +6,24 @@ class HYRequest{
 
     constructor(config: AxiosRequestConfig){
         this.instance = axios.create(config)
+        // 给每个instance实例都添加拦截器（全局拦截器）
+        this.instance.interceptors.request.use(
+            (config) => {
+                // loading/token
+                return config
+            },
+            (err) => {
+                return err
+            }
+        )
+        this.instance.interceptors.response.use(
+            (res) => {
+                return res.data
+            },
+            (err) => {
+                return err
+            }
+        )
     }
 
     request(config:AxiosRequestConfig){
@@ -15,6 +33,7 @@ class HYRequest{
     get(config:AxiosRequestConfig){
         return this.request({...config,method:"GET"})
     }
+
 
     post(config:AxiosRequestConfig){
         return this.request({...config,method:"POST"})
